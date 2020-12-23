0d2532bbfb17844acf351469e74c8e961f5a35c4
hmg612 <hmgcjn@gmail.com>
2020-12-24 01:35:35 +0900

    source code 12/23
    mvc code making & subscribe function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, render_template, make_response
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_cors import CORS
 from blog_view import blog
+from blog_control.user_mgmt import User
 import os
 
 # http 만을 지원하는 기능을 http에서 테스트할 때 필요
@@ -9,7 +10,7 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__, static_url_path='/static')
 CORS(app)
-app.secure_key = 'admin_server'
+app.secret_key = 'admin_server'
 
 app.register_blueprint(blog.blog_abtest, url_prefix='/blog')
 login_manager = LoginManager()
