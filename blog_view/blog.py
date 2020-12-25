4b5be13eaf6798bc07d7e62ef3666671c7e9258e
hmg612 <hmgcjn@gmail.com>
2020-12-26 01:03:37 +0900

    12/25 code
    user log collecting by mongodb

--- blog_view/blog.py
+++ blog_view/blog.py
@@ -1,5 +1,6 @@
-from flask import Flask, Blueprint, request, render_template, make_response, jsonify, redirect, url_for
+from flask import Flask, Blueprint, request, render_template, make_response, jsonify, redirect, url_for, session
 from blog_control.user_mgmt import User
+from blog_control.session_mgmt import BlogSession
 from flask_login import login_user, current_user, logout_user
 import datetime
 
@@ -15,28 +16,35 @@ def set_email():
     else:
         print('set_email', request.headers)
         # content type이 application/json인 경우에만 request.get_json() 호출 가능
-        #print('set_email', request.get_json())
-        print('set_email', request.form['user_email'])
-        user = User.create(request.form['user_email'], 'A')
+        # print('set_email', request.get_json())
+        # print('set_email', request.form['user_email'])
+        # print('set_email', request.form['blog_id'])
+        user = User.create(request.form['user_email'], request.form['blog_id'])
         login_user(user, remember=True, duration=datetime.timedelta(
             days=365))  # session/cookie 생성객체
 
-        return redirect(url_for('blog.test_blog'))
+        return redirect(url_for('blog.blog_fullstack'))
 
     # return redirect('/blog/test_blog')
     # return make_response(jsonify(success=True), 200)
 
 
-@blog_abtest.route('/test_blog')
-def test_blog():
+@blog_abtest.route('/blog_fullstack')
+def blog_fullstack():
     if current_user.is_authenticated:
-        return render_template('blog_A.html', user_email=current_user.user_email)
+        webpage_name = BlogSession.get_blog_page(current_user.blog_id)
+        BlogSession.save_session_info(
+            session['client_id'], current_user.user_email, webpage_name)
+        return render_template(webpage_name, user_email=current_user.user_email)
     else:
-        return render_template('blog_A.html')
+        webpage_name = BlogSession.get_blog_page()
+        BlogSession.save_session_info(
+            session['client_id'], 'anonymous', webpage_name)
+        return render_template(webpage_name)
 
 
 @blog_abtest.route('/logout')
 def logout():
     User.delete(current_user.id)
     logout_user()
-    return redirect(url_for('blog.test_blog'))
+    return redirect(url_for('blog.blog_fullstack'))
